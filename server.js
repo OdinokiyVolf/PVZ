afacee8f6fa4f7155d3b5198ee509e51a3da56f1
OdinokiyVolf <123843210+OdinokiyVolf@users.noreply.github.com>
2026-05-03 16:53:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,14 @@
 const express = require("express");
-const sqlite3 = require("sqlite3").verbose();
-const bodyParser = require("body-parser");
-
+const path = require("path");
 const app = express();
-const db = new sqlite3.Database("db.sqlite");
 
-app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname));
 
-// создаём таблицу
-db.run(`
-CREATE TABLE IF NOT EXISTS stats (
-  id TEXT PRIMARY KEY,
-  count INTEGER
-)
-`);
-
-// получить статистику
-app.get("/api/stats", (req, res) => {
-  db.all("SELECT * FROM stats", (err, rows) => {
-    res.json(rows);
-  });
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// клик
-app.post("/api/click", (req, res) => {
-  const { id } = req.body;
-
-  db.run(`
-    INSERT INTO stats (id, count)
-    VALUES (?,1)
-    ON CONFLICT(id) DO UPDATE SET count = count + 1
-  `, [id]);
-
-  res.sendStatus(200);
-});
-
-app.listen(3000, () => {
-  console.log("🚀 Сервер запущен: http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Сервер запущен на http://localhost:${PORT}`);
 });
